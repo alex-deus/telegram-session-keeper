8fc844fcf9885db703581149477b648ac5da616f
Alex Deus <aleksei@aleksei.local>
2025-12-26 20:47:53 +0100

    Used sh at docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,16 +1,15 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-args=("$@")
-
-case "${1}" in
-    "bash")
-        shift
-        exec bash -c "${args[@]:1}"
-        ;;
-    "sleep")
-        exec bash -c "while true; do sleep 20; done"
-        ;;
-    "cli")
-        exec ./telegram_session_keeper/cli.py ${args[@]:1}
-        ;;
+case "$1" in
+  shell)
+    shift
+    exec sh -c "$*"
+    ;;
+  sleep)
+    exec sh -c "while true; do sleep 20; done"
+    ;;
+  cli)
+    shift
+    exec ./telegram_session_keeper/cli.py "$@"
+    ;;
 esac
